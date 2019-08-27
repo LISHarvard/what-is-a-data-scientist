6c1097b3bad6544635fc9ee6530614fe1d9ea598
Haylee Ham <hayleeham@gmail.com>
2019-08-27 12:44:41 -0400

    multinomial regression for time spent on job title

--- logit_compensation.R
+++ logit_compensation.R
@@ -6,16 +6,23 @@ library(nnet)
 df = read.csv("/Users/hayleeham/Documents/what-is-a-data-scientist/compensation_data.csv")
 
 # how does time spent affect compensation
-model_fit <- polr(Q9~Q34_Part_1+Q34_Part_2+Q34_Part_3+Q34_Part_4+Q34_Part_5+gender+usa+india+china+tech+academia+exp, data = df, Hess = TRUE)
-summary(model_fit)
-# create the p values
-summary_table <- coef(summary(model_fit))
-pval <- pnorm(abs(summary_table[, "t value"]),lower.tail = FALSE)* 2
-summary_table <- cbind(summary_table, "p value" = round(pval,3))
-summary_table
+# model_fit <- polr(Q9~Q34_Part_1+Q34_Part_2+Q34_Part_3+Q34_Part_4+Q34_Part_5+gender+usa+india+china+tech+academia+exp, data = df, Hess = TRUE)
+# summary(model_fit)
+# # create the p values
+# summary_table <- coef(summary(model_fit))
+# pval <- pnorm(abs(summary_table[, "t value"]),lower.tail = FALSE)* 2
+# summary_table <- cbind(summary_table, "p value" = round(pval,3))
+# summary_table
+
+# try running the model as a simple OLS using the averages from the buckets
+model_fit_ols <- lm(avg~clean_data+data_viz+build_model+model_prod+comm_insights+female+usa+india+china+tech+academia+exp+Data.Scientist+Software.Engineer+Data.Analyst+Research.Scientist+Consultant+Data.Engineer+Business.Analyst+Manager+Research.Assistant, data = df)
+summary(model_fit_ols)
+
+# read in data
+df10 = read.csv("/Users/hayleeham/Documents/what-is-a-data-scientist/compensation_data_10.csv")
 
 # how about how time spent affects job title
-model_fit_title <- multinom(Q6~Q34_Part_1+Q34_Part_2+Q34_Part_3+Q34_Part_4+Q34_Part_5+gender+usa+india+china+tech+academia+exp, data = df)
+model_fit_title <- multinom(Q6~clean_data+data_viz+build_model+model_prod+comm_insights+female+usa+india+china+tech+academia+exp, data = df10)
 summary(model_fit_title)
 # create the p values
 z <- summary(model_fit_title)$coefficients/summary(model_fit_title)$standard.errors
